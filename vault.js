755be4ceda977237a64a8f51fea983742539868f
KELangsdorf <kelangsdorf@gmail.com>
2024-03-11 21:51:16 -0400

    Vault

---
+++ vault.js
@@ -0,0 +1,31 @@
+//This console.log was a test to try out if the html was linked.
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+//Letters as Numbers (For Equations):
+const A = 7;
+const B = 15;
+const C = 30 - 20;
+const D = 17;
+//Animals as Numbers (For Equations):
+let Monkey = 20;
+let Zebra = 25 * 2;
+let Tiger = 30;
+let Jaguar = 8 + 2;
+let Gator = 16 / 2;
+let Wolf = 2;
+// Equations for the Alerts to make the alerts easier.
+let comboOne = D - A;
+let comboTwo = Jaguar - Tiger + Zebra + Wolf + Gator;
+let comboThree = D + Wolf + Tiger - C;
+//This was to assess if the variables equated to the combination.
+console.log("The Combination is:");
+console.log("First Number of Combination:")
+console.log(comboOne)
+console.log("Second Number of Combination")
+console.log(comboTwo)
+console.log("Third Number of the Combination")
+console.log(comboThree)
+//The Vault Code Alerts to pop up on the HTML page.
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+alert("First Number of Combination: " + comboOne)
+alert("Second Number of Combination: " + comboTwo)
+alert("Third Number of the Combination: " + comboThree)
